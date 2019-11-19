f6a2ab59a6aa00780244263782484218683357dc
rick <rickwestdev@gmail.com>
2019-11-19 00:43:23 +0000

    Implement basic proof of work

--- blockchain/block.py
+++ blockchain/block.py
@@ -1,6 +1,7 @@
 import time
 import json
 import hashlib
+from config import MINE_RATE
 
 
 class Block:
@@ -11,13 +12,17 @@ class Block:
         "last_hash": "a4dd70bc5bb3ac23bc3b829a3b37029d15c7530c318ffcff7f30691b498745df",
         "hash": "9725bdb821e618a50dbfe245a8c4b7c21f73ff0ee16480eb1c51f94344a40aa4",
         "data": [],
+        'difficulty': 3,
+        'nonce': 'genesis_nonce'
     }
 
-    def __init__(self, timestamp, last_hash, hash, data):
+    def __init__(self, timestamp, last_hash, hash, data, difficulty, nonce):
         self.timestamp = timestamp
         self.last_hash = last_hash
         self.hash = hash
         self.data = data
+        self.difficulty = difficulty
+        self.nonce = nonce
 
     def __repr__(self):
         return """
@@ -25,23 +30,52 @@ class Block:
             last hash: {}
             hash: {}
             data: {}
+            difficulty: {}
+            nonce: {}
             """.format(
-            self.timestamp, self.last_hash, self.hash, self.data
+            self.timestamp, self.last_hash, self.hash, self.data, self.difficulty, self.nonce
         )
 
     @staticmethod
     def mine(last_block, data):
-        """Mines a block based on given last block and data."""
+        """
+        Mines a block based on given last block and data.
+        Mining is only complete when a block hash is found that meets the leading 0's requirement of proof of work.
+        """
         timestamp = time.time_ns()
         last_hash = last_block.hash
-        hash = BlockHelper.hash(timestamp, last_hash, data)
 
-        return Block(timestamp, last_hash, hash, data)
+        difficulty = Block.regulate_difficulty(last_block, timestamp)
+        nonce = 0
+
+        hash = BlockHelper.hash(timestamp, last_hash, data, difficulty, nonce)
+
+        while hash[0:difficulty] != '0' * difficulty:
+            nonce +=1
+            timestamp = time.time_ns()
+            difficulty = Block.regulate_difficulty(last_block, timestamp)
+            hash = BlockHelper.hash(timestamp, last_hash, data, difficulty, nonce)
+
+        return Block(timestamp, last_hash, hash, data, difficulty, nonce)
 
     @staticmethod
     def genesis():
         """Genesis Block"""
         return Block(**Block.GENESIS)
+
+    @staticmethod
+    def regulate_difficulty(last_block, new_timestamp):
+        """Regulates the block difficulty based on the configured MINE_RATE.
+        We increase the difficulty for blocks mined too quickly and decrease the difficulty for blocks mined slowly
+        """
+        difficulty = last_block.difficulty
+
+        if (new_timestamp - last_block.timestamp) < MINE_RATE:
+            return difficulty + 1
+        elif (difficulty - 1) > 0:
+            return difficulty - 1
+        else:
+            return 1
 
 
 class BlockHelper:
--- tests/blockchain/test_block.py
+++ tests/blockchain/test_block.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 from blockchain.block import Block
+from config import MINE_RATE, SECONDS
+import time
 
 
 class TestBlock(TestCase):
@@ -12,6 +14,41 @@ class TestBlock(TestCase):
         self.assertIsInstance(result, Block)
         self.assertEqual(result.data, data)
         self.assertEqual(result.last_hash, last_block.hash)
+        self.assertEqual(result.hash[0:result.difficulty], '0' * result.difficulty)
+
+    def test_difficulty_when_block_mined_too_quickly(self):
+        last_block = Block.mine(Block.genesis(), 'Test data')
+
+        # New block mined immediately, quicker than the MINE_RATE.
+        mined_block = Block.mine(last_block, 'Some more data')
+
+        # Assert that difficulty has increased by increment of one
+        self.assertEqual(mined_block.difficulty, last_block.difficulty + 1)
+
+    def test_difficulty_when_block_mined_too_slowly(self):
+        last_block = Block.mine(Block.genesis(), 'Test data')
+
+        time.sleep(MINE_RATE / SECONDS)
+
+        # New block mined too slowly, slower that the MINE_RATE.
+        mined_block = Block.mine(last_block, 'Some more data')
+
+        # Assert that difficulty has decreased by increment of one
+        self.assertEqual(mined_block.difficulty, last_block.difficulty - 1)
+
+    def test_mine_block_difficulty_not_less_than_1(self):
+        last_block = Block.mine(Block.genesis(), 'Test data')
+        # Ensure last block difficulty is 1
+        last_block.difficulty = 1
+
+        time.sleep(MINE_RATE / SECONDS)
+
+        # New block mined too slowly, slower that the MINE_RATE.
+        mined_block = Block.mine(last_block, 'Some more data')
+
+        # Assert than even though the block mined slowly,
+        # the difficulty is not decreased as it cannot be less than 1
+        self.assertEqual(mined_block.difficulty, 1)
 
     def test_genesis(self):
         expected = Block(
@@ -19,6 +56,8 @@ class TestBlock(TestCase):
             Block.GENESIS["last_hash"],
             Block.GENESIS["hash"],
             Block.GENESIS["data"],
+            Block.GENESIS["difficulty"],
+            Block.GENESIS["nonce"],
         )
 
         result = Block.genesis()
